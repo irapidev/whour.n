88a7fa2a7f67cca9b1e4c48b998c4eb8eca07008
rapidevelop <iman.v846@gmail.com>
2019-03-28 01:20:48 +0430

    build(babel): install Babel and configure it to transpile TypeScript

---
+++ babel.config.js
@@ -0,0 +1,25 @@
+module.exports = function (api) {
+
+  api.cache(true);
+
+  const presets = [
+    [
+      '@babel/preset-env',
+      {
+        'targets': {
+          'node': 'current'
+        }
+      }
+    ],
+    '@babel/preset-typescript'
+  ];
+  const plugins = [
+    '@babel/proposal-class-properties',
+    '@babel/proposal-object-rest-spread'
+  ];
+
+  return {
+    presets,
+    plugins
+  };
+}
